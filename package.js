2324c2aaeb098361256220b1ba67fdbe830851e8
lspellman <lance.spellman@workflowstudios.com>
2015-01-25 16:37:33 -0600

    Update package.js
    Make an English version available.

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.on_use(function (api) {
   var path = Npm.require('path');
   var asset_path = path.join('lib');
   api.add_files(path.join(asset_path, 'css', 'ui.jqgrid.css'), 'client');
-  api.add_files(path.join(asset_path, 'js', 'i18n/grid.locale-es.js'), 'client');
+  api.add_files(path.join(asset_path, 'js', 'i18n/grid.locale-en.js'), 'client');
   api.add_files(path.join(asset_path, 'js', 'jquery.jqGrid.min.js'), 'client');
 
 });
